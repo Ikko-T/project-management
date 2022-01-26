80c647c61e88c6b021cd219b17bb1be19ebdc7d6
Ikko-T <skyblueboy32@gmail.com>
2022-01-26 19:55:24 +0900

    Set up validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,6 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
 
-
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :description, length: { maximum: 500 }
 end
