c44d91111ef3ba9bba0788d725455048c1218ea3
Ikko-T <skyblueboy32@gmail.com>
2022-01-29 18:06:35 +0900

    Change a datatype of the status column to integer from string

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,11 @@ class Task < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, length: { maximum: 500 }
+  validates :status, inclusion: { within: %w(not_started on_going complete) }
+
+  enum status: {
+    not_started: 0,
+    on_going: 1,
+    complete: 2,
+  }
 end
